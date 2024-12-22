a520ad83260cab89660dbb3c273c36c87c67d06d
Ubongpr7 <ubongpr7@gmail.com>
2024-12-21 23:02:13 -0800

    Scene frontend job

--- mainapps/video/views.py
+++ mainapps/video/views.py
@@ -470,6 +470,6 @@ def fetch_video_categories(request):
     return JsonResponse(list(categories), safe=False)
 
 def get_clip(request, cat_id):
-    category = get_object_or_404(ClipCategory, id=cat_id)
+    category = get_object_or_404(ClipCategory, id=cat_id).values("id", "title", )
     videos = VideoClip.objects.filter(category=category)
-    return render(request, "partials/model_options.html", {"items": videos})
+    return JsonResponse(list(videos), safe=False)
