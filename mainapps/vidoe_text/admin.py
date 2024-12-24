988b6741e45ac795445d70cf058e787f99b42f5a
Ubong Prosper <ubongpr7@gmail.com>
2024-12-24 23:26:21 +0100

    close btn

--- mainapps/vidoe_text/admin.py
+++ mainapps/vidoe_text/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import TextFile, TextLineVideoClip, LogoModel
+from .models import TextFile, TextLineVideoClip, LogoModel,SubClip
 
 admin.site.register(TextLineVideoClip)
 admin.site.register(TextFile)
 admin.site.register(LogoModel)
+admin.site.register(SubClip)
