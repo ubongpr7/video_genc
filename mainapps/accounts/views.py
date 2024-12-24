36411f9617fa9ec8145e9589220068d790bc05fa
Ubong Prosper <ubongpr7@gmail.com>
2024-12-24 21:23:26 +0100

    close btn

--- mainapps/accounts/views.py
+++ mainapps/accounts/views.py
@@ -204,9 +204,10 @@ def register(request):
             except Exception as _:
                 messages.error(request, "Subscription Failed. Please Try Again Later.")
                 return redirect(
-                    reverse(
-                        "accounts:register", kwargs={"session_id": checkout_session_id}
-                    )
+                    f'accounts/register/?session_id={checkout_session_id}'
+                    # reverse(
+                    #     "accounts:register", kwargs={"session_id": checkout_session_id}
+                    # )
                 )
 
             send_html_email2(
