7208f208dc929aaa32b79e80d57b072b281840b8
Ubong Prosper <ubongpr7@gmail.com>
2024-12-30 13:59:01 +0100

    Text file display

--- mainapps/vidoe_text/forms.py
+++ mainapps/vidoe_text/forms.py
@@ -2,19 +2,6 @@ from django import forms
 from .models import TextFile
 
 
-class TextFileForm(forms.ModelForm):
-    # Resolution Options
-    RESOLUTION_CHOICES = [
-        ("1:1", "FB/IG feed"),
-        ("16:9", "YouTube"),
-    ]
-    resolution = forms.ChoiceField(choices=RESOLUTION_CHOICES, initial="1:1")
-
-    class Meta:
-        model = TextFile
-        fields = "__aLL__"
-# forms.py
-
 from django import forms
 
 class TextFileUpdateForm(forms.ModelForm):
